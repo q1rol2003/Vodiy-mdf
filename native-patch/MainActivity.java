078b8c919d5089e25d13dfd2a8156aa5fad760ae
q1rol2003 <158846728+q1rol2003@users.noreply.github.com>
2026-09-10 08:38:55 +0500

    Implement crash log display in MainActivity
    Added a method to display crash logs in an AlertDialog if they exist.

--- native-patch/MainActivity.java
+++ native-patch/MainActivity.java
@@ -1,7 +1,12 @@
 package com.vodiymdf.app;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 
 public class MainActivity extends BridgeActivity {
 
@@ -24,5 +29,31 @@ public class MainActivity extends BridgeActivity {
 
         registerPlugin(MicRecorderPlugin.class);
         super.onCreate(savedInstanceState);
+
+        showCrashLogIfExists();
+    }
+
+    private void showCrashLogIfExists() {
+        try {
+            File logFile = new File(getExternalFilesDir(null), "crash_log.txt");
+            if (logFile.exists()) {
+                StringBuilder sb = new StringBuilder();
+                BufferedReader br = new BufferedReader(new FileReader(logFile));
+                String line;
+                while ((line = br.readLine()) != null) {
+                    sb.append(line).append("\n");
+                }
+                br.close();
+
+                new AlertDialog.Builder(this)
+                    .setTitle("Oxirgi xatolik")
+                    .setMessage(sb.toString())
+                    .setPositiveButton("Yopish", (d, w) -> logFile.delete())
+                    .setCancelable(true)
+                    .show();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
